f36dd1de6e88d80eef01977f94de6c72159a5b27
BJarv <brandon.jarv@gmail.com>
2015-02-27 16:00:07 -0800

    tyler's stuff

---
+++ Assets/Scripts/Standard Assets/CharControl.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum JumpState
+{
+	GROUNDED,
+	JUMPING,
+	FALLING
+}
+
+public class CharControl : MonoBehaviour {
+
+	//Character controls
+	KeyCode moveLeft;
+	KeyCode moveRight;
+	KeyCode jump;
+	KeyCode dashLeft;
+	KeyCode dashRight;
+
+	//Movement variables
+	public float maxSpeed = 100f;
+	public float addSpeed = 1000f;
+	private JumpState Jump = JumpState.GROUNDED;
+	
+	//Ground stuff
+	[HideInInspector] public Transform groundCheck;
+	float raycastLength = 0.15f;
+	public LayerMask whatIsGround;
+
+	//Jumpforce variables
+	public float PlusJumpForce = 5f;
+	public float CurrJumpForce = 0f;
+	public float MaxJumpForce = 10f;
+
+	public int horizDirection = 1;
+
+	float moveH;
+	//control bools
+	public bool colliding = false;
+	public bool dashLock = false;
+	public bool isDashing = false;
+	public bool singleDamageDealt = false;
+
+	//cooldown timers
+	public float lockTimeStamp = 0;
+	public float dashTimeStamp = 0;
+	public float damageTimeStamp = 0;
+
+	//Dash attack variables
+	public float dashX = 5f;
+	public float dashY = 5f;
+
+
+	GameObject player;
+	PlayerStats playerStats;
+
+
+
+	void Awake () {
+		var playerList = GameObject.FindGameObjectsWithTag ("Player");
+		if(this.name.Equals ("Player1")){
+			player = playerList[0];
+		}else if (this.name.Equals ("Player2")){
+			player = playerList[1];
+		}
+		playerStats = player.GetComponent <PlayerStats> ();
+
+		Debug.Log (this.name);
+	}
+
+	void Start () {
+		assignPlayerControls ();
+		groundCheck = transform.Find ("groundCheck").transform;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (!playerStats.isDead) {
+			float moveH = 0; //= Input.GetAxis ("Horizontal");
+
+			if (Input.GetKey(moveRight) || Input.GetKey (moveLeft)) {
+				if( Input.GetKey(moveRight) ) {
+					moveH = 1;
+				} else {
+					moveH = -1;
+				}
+			}
+
+			Flip (moveH);
+			if (moveH > 0) {
+				if (rigidbody2D.velocity.x <= maxSpeed)
+					rigidbody2D.AddForce (new Vector2 (moveH * addSpeed, 0));
+			} else if (moveH == 0); //set animation to idle here.
+		    else if (moveH < 0) {
+				if (rigidbody2D.velocity.x > -maxSpeed)
+					rigidbody2D.AddForce (new Vector2 (moveH * addSpeed, 0));
+			}
+			DashAttack (); //Player can dash in any JumpState
+			dealDamage();
+		}
+	}
+
+	void Update() {
+	  if (!playerStats.isDead) {
+			switch (Jump) {
+			case JumpState.GROUNDED: 
+			//	dashLock = false;
+				if (Input.GetKey (jump) && isGrounded ()) {
+					Jump = JumpState.JUMPING;
+				}
+			//DashAttack();
+				break;
+			
+			case JumpState.JUMPING: 
+				if (Input.GetKey (jump) && CurrJumpForce < MaxJumpForce) {
+					var timeDiff = Time.deltaTime * 10;
+					var forceToAdd = PlusJumpForce * timeDiff;
+					CurrJumpForce += forceToAdd;
+					rigidbody2D.AddForce (new Vector2 (0, forceToAdd));
+				} else {
+					Jump = JumpState.FALLING;
+					CurrJumpForce = 0;
+				}
+			//DashAttack();
+				break;
+			
+			case JumpState.FALLING: 
+				if (isGrounded () && rigidbody2D.velocity.y <= 0) {
+					Jump = JumpState.GROUNDED;
+				}
+			//DashAttack ();
+				break;
+			
+			}
+			lockfuction();
+
+
+		}
+	}
+
+
+	void DashAttack(){
+
+			float xTotal = 0f;
+			float yTotal = 0f;
+			if (!dashLock) { //player can only dash once per jump
+				if (Input.GetKey (dashRight)) {
+					Flip (1);
+					//rigidbody2D.AddForce (new Vector2 (100, 0));
+					xTotal = dashX;
+					//playerStats.currentHealth-=10;
+				    lockTimeStamp = Time.time + 1.5f;
+					dashTimeStamp = Time.time + .4f;
+					dashLock = true;
+					isDashing = true;
+				} else if (Input.GetKey (dashLeft)) {
+					Flip (-1);
+					//rigidbody2D.AddForce (new Vector2 (-100, 0));
+					xTotal = -dashX;
+					lockTimeStamp = Time.time + 1.5f;
+					dashTimeStamp = Time.time + .4f;
+					//playerStats.currentHealth-=10;
+					dashLock = true;
+					isDashing = true;
+				}
+				//dashLock = true;
+
+			//Jump = JumpState.FALLING;
+			//CurrJumpForce = 0;
+			rigidbody2D.AddForce (new Vector2 (xTotal, yTotal));
+		}
+
+	//	if(player.name.Equals ("Player1")
+	}
+	void assignPlayerControls(){
+		if(player.name.Equals("Player1")){
+			moveLeft = KeyCode.A;
+			moveRight = KeyCode.D;
+			jump = KeyCode.W;
+			dashLeft = KeyCode.Q;
+			dashRight = KeyCode.E;
+			Debug.Log ("Player 1 Controls Set");
+		}
+		else if (player.name.Equals("Player2")){
+			moveLeft = KeyCode.J;
+			moveRight = KeyCode.L;
+			jump = KeyCode.I;
+			dashLeft = KeyCode.U;
+			dashRight = KeyCode.O;
+			Debug.Log ("Player 2 Controls Set");
+		}
+	}
+
+	// 
+	void lockfuction(){
+		if (lockTimeStamp <= Time.time){
+			dashLock = false;
+		}
+		if (dashTimeStamp <= Time.time) {
+			isDashing = false;
+		}
+		if (damageTimeStamp <= Time.time) {
+			singleDamageDealt = false;
+		}
+	}
+
+	void dealDamage() {
+		if ((colliding == true) && (isDashing == true) && (singleDamageDealt == false)) {
+			playerStats.currentHealth-=10;
+		//	playerStats.currentHealth-=10;
+			singleDamageDealt = true;
+			damageTimeStamp = Time.time + .5f;
+		}
+	}
+	void Flip(float moveH)
+	{
+		if (moveH > 0)
+			transform.localEulerAngles = new Vector3 (0, 0, 0);
+		else if (moveH < 0)
+			transform.localEulerAngles = new Vector3 (0, 180, 0);
+	}
+
+	public bool isGrounded()
+	{
+		return Physics2D.Raycast (groundCheck.position, -Vector2.up, raycastLength, whatIsGround);
+	}
+
+
+}
